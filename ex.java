ee342f85d028474dd8831eec4ed4ca958f16c671
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-05-07 19:47:47 -0300

    Update ex.java

--- ex.java
+++ ex.java
@@ -1,9 +1,14 @@
 public class ex{
   public static void public static void main(String[] args) {
     /*
-     * Esse é um
-     * teste de comentário
-     * fdsfdsfds
+     * Programa que soma 2 números
+     *  1- apontar os dois números
+     *  2- somar os dois números
+     *  3- imprimir a soma
      */
+    int num1, num2;
+    num=5;
+    num2=10
+
   }
 }
