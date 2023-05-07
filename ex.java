3f7fc826c82fb5f49fd8c8fec7bc671052829b74
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-05-07 19:50:26 -0300

    Update ex.java

--- ex.java
+++ ex.java
@@ -6,9 +6,16 @@ public class ex{
      *  2- somar os dois números
      *  3- imprimir a soma
      */
+    // parte 1
     int num1, num2;
-    num=5;
+    num1=5;
     num2=10
+
+    // parte 2
+    int soma= num1+num2
+
+    // parte 3
+    System.out.println("O resultado da soma é"soma);
 
   }
 }
