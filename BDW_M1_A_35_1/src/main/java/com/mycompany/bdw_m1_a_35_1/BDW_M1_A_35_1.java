72a64f15592992381488c95f9528f243e5c20694
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-05-08 14:48:21 -0300

    create

---
+++ BDW_M1_A_35_1/src/main/java/com/mycompany/bdw_m1_a_35_1/BDW_M1_A_35_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bdw_m1_a_35_1;
+
+/**
+ *
+ * @author dudah
+ */
+public class BDW_M1_A_35_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
