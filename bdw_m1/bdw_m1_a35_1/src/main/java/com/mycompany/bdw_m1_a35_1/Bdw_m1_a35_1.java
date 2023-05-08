1470ce1e70656423fd4b3a46cd7827ef0ee48f77
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-05-08 18:15:21 -0300

    Update Bdw_m1_a35_1.java

--- bdw_m1/bdw_m1_a35_1/src/main/java/com/mycompany/bdw_m1_a35_1/Bdw_m1_a35_1.java
+++ bdw_m1/bdw_m1_a35_1/src/main/java/com/mycompany/bdw_m1_a35_1/Bdw_m1_a35_1.java
@@ -5,6 +5,7 @@ public class Bdw_m1_a35_1 {
     public static void main(String[] args) {
         int num1=5, num2=15;
         int soma=num1+num2;
+        Scanner Entrada= new Scanner (System.in);
         
         System.out.println("A soma dos números é: "+soma);
     }
