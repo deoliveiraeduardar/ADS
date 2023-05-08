190a68fbe4ceb84fe8ce867ae9b6b2647f0d8e37
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-05-08 17:51:30 -0300

    Create

---
+++ bdw_m1/bdw_m1_a35_1/src/main/java/com/mycompany/bdw_m1_a35_1/Bdw_m1_a35_1.java
@@ -0,0 +1,11 @@
+package com.mycompany.bdw_m1_a35_1;
+
+public class Bdw_m1_a35_1 {
+
+    public static void main(String[] args) {
+        int num1=5, num2=15;
+        int soma=num1+num2;
+        
+        System.out.println("A soma dos números é: "+soma);
+    }
+}
