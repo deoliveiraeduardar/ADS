92bacd36cf8d1d0881fa01e15b94a6cd5a652f89
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-05-08 22:45:12 -0300

    create bdw

--- bdw_m1/Bdw_m1_a42_1/src/main/java/com/mycompany/bdw_m1_a42_1/Bdw_m1_a42_1.java
+++ bdw_m1/Bdw_m1_a42_1/src/main/java/com/mycompany/bdw_m1_a42_1/Bdw_m1_a42_1.java
@@ -1,9 +1,19 @@
 package com.mycompany.bdw_m1_a42_1;
-
+/* 1. Criar variáveis
+   2. Avaliar critérios
+   3. Imprimir respostas
+*/
 
 public class Bdw_m1_a42_1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        boolean renda=true;
+        boolean adimplencia=false;
+        boolean privilege=false;
+        boolean restricao=true;
+        
+        boolean resultado=((renda&&adimplencia)||privilege);
+         
+        System.out.println("Empréstimo deve ser concedido? "+resultado);
     }
 }
