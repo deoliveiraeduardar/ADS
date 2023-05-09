c3c2b120936db2720b24e0edd75cd27fc312c590
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-05-09 08:06:35 -0300

    Update Bdw_m1_tp_1_programa2.java

--- bdw_m1/Bdw_m1_tp_1_programa2/src/main/java/com/mycompany/bdw_m1_tp_1_programa2/Bdw_m1_tp_1_programa2.java
+++ bdw_m1/Bdw_m1_tp_1_programa2/src/main/java/com/mycompany/bdw_m1_tp_1_programa2/Bdw_m1_tp_1_programa2.java
@@ -11,15 +11,22 @@ public class Bdw_m1_tp_1_programa2 {
         num2+=num1;
         if((num1%2)==0){
             num3=num1*4;
+            System.out.println("Condição 1, if((num1%2)==0     é V ");
+
         }else if ((num2%3)==0){
             num3=num2+2*num1;
+            System.out.println("Condição 2, else if ((num2%3)==0)     é V ");
+
         }else{
             num3=num2;
+            System.out.println("Condição 3, else    é V ");
+         
         }
-        
-       System.out.println("       O valor de num2 é= "+num2);
-       System.out.println("1. O valor de num3 é= "+num3);
+       System.out.println("===========================================");
 
-        
+       System.out.println("1. O valor de num3 é= "+num3);
+       System.out.println("        - O valor de num2 é= "+num2);
+       
+
     }
 }
