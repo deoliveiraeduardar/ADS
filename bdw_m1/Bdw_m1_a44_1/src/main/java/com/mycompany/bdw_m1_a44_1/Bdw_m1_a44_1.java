f814f7e183b16665cb58cafd12657228a9231b58
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-05-08 23:34:18 -0300

    default

--- bdw_m1/Bdw_m1_a44_1/src/main/java/com/mycompany/bdw_m1_a44_1/Bdw_m1_a44_1.java
+++ bdw_m1/Bdw_m1_a44_1/src/main/java/com/mycompany/bdw_m1_a44_1/Bdw_m1_a44_1.java
@@ -12,10 +12,38 @@ public class Bdw_m1_a44_1 {
 
     public static void main(String[] args) {
         int dia;
-        Scanner Entrada= new Scanner(system.in);
+        Scanner Entrada=new Scanner(System.in);
         
-        System.out.println("Digite o dia: ");
-        dia=Entrada.next.int();
+        System.out.println("Qual o número do dia? (1-7): ");
+        dia=Entrada.nextInt();
+        
+        switch(dia){
+            case 1:
+                System.out.println("Segunda");
+                break;
+            case 2:
+                System.out.println("Terça");
+                break;
+            case 3:
+                System.out.println("Quarta");
+                break;        
+            case 4:
+                System.out.println("Quinta");
+                break;               
+            case 5:
+                System.out.println("Sexta");
+                break;   
+             case 6:
+                System.out.println("Sábado");
+                break;                  
+            case 7:
+                System.out.println("Domingo");
+                break;   
+            default:
+                System.out.println("NÚMERO INVÁLIDO");
+                
+                
+        }
         
 
     }
