be9549451ca603c4408c0464e17454dc604d984b
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-05-08 23:17:11 -0300

    Update Bdw_m1_a43_1.java

--- bdw_m1/Bdw_m1_a43_1/src/main/java/com/mycompany/bdw_m1_a43_1/Bdw_m1_a43_1.java
+++ bdw_m1/Bdw_m1_a43_1/src/main/java/com/mycompany/bdw_m1_a43_1/Bdw_m1_a43_1.java
@@ -12,7 +12,7 @@ public class Bdw_m1_a43_1 {
 
     public static void main(String[] args) {
         
-        double peso, altura;
+        double peso, altura, imc;
         Scanner Entrada= new Scanner(System.in);
         
         System.out.println("Digite o peso: ");
@@ -21,13 +21,21 @@ public class Bdw_m1_a43_1 {
         System.out.println("Digite a altura: ");
         altura=Entrada.nextDouble();
         
-        double imc=((peso)/(altura*altura));
+        imc=peso/(altura*altura);
         System.out.println("O IMC é: "+imc);
         
+        if(imc<18.5){
+            System.out.println("Abaixo do peso");
+        }else if((imc>=18.5)&&(imc<=24.9)){
+            System.out.println("Peso normal");
+        }else if((imc>24.9)&&(imc<=29.9)){
+            System.out.println("Sobrepeso");
+        }else {
+            System.out.println("Obesidade");
+            
+        }
         
-                
-                
-                
+              
 
     }
 }
