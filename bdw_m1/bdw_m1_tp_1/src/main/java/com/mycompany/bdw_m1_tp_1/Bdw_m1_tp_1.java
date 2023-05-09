5b8f71fe168e9fbd4ebd2ea0e852cdede9f121b8
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-05-09 07:00:48 -0300

    lines ok

--- bdw_m1/bdw_m1_tp_1/src/main/java/com/mycompany/bdw_m1_tp_1/Bdw_m1_tp_1.java
+++ bdw_m1/bdw_m1_tp_1/src/main/java/com/mycompany/bdw_m1_tp_1/Bdw_m1_tp_1.java
@@ -6,19 +6,15 @@ public class Bdw_m1_tp_1 {
         int num1, num2;
         double div=0;
         boolean ok=false;
-        
         Scanner Entrada= new Scanner (System.in);
-        System. out .println("Digite um numero inteiro maior que 100 (ex: 150).");
+        System. out .println("Digite um numero inteiro maior que 100 (ex: 150): ");
         num1=Entrada.nextInt();
-
-        System. out .println("Digite um número inteiro menor que 10 (ex: 6)");
+        System. out .println("Digite um número inteiro menor que 10 (ex: 6): ");
         num2=Entrada.nextInt();
-               
-        
         if((num1>100)&&(num2<10)) {
             System.out.println("Muito bem, valores válidos.");
             ok=true;
-            div=num/num2;
+            div=num1/num2;
         }else if (((num1<100)&&(num2<10)) || ((num1>100)&&(num2>10))){
             System.out.println("Algum dos números está fora do limite.");
         }else {
