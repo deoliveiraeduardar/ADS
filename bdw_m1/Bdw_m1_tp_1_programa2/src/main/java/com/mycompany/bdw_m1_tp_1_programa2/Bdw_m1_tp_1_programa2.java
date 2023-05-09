eaded3f0195305aed2f42efc063c055b024b8322
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-05-09 07:32:18 -0300

    Update Bdw_m1_tp_1_programa2.java

--- bdw_m1/Bdw_m1_tp_1_programa2/src/main/java/com/mycompany/bdw_m1_tp_1_programa2/Bdw_m1_tp_1_programa2.java
+++ bdw_m1/Bdw_m1_tp_1_programa2/src/main/java/com/mycompany/bdw_m1_tp_1_programa2/Bdw_m1_tp_1_programa2.java
@@ -1,16 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.bdw_m1_tp_1_programa2;
-
-/**
- *
- * @author dudah
- */
+import java.util.Scanner;
 public class Bdw_m1_tp_1_programa2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int num1, num2=20, num3;
+
+        Scanner Entrada= new Scanner (System.in);
+        
+        System. out .println("Digite um número qualquer: ");
+        num1=Entrada.nextInt();
+        num2+=num1;
+        if((num1%2)==0){
+            num3=num1*4;
+            
+        }else if ((num2%3)==0){
+            num3=num2+2*num1;
+            
+        }else{
+            num3=num2;
+        }
+        System.out.println("O valor de num3 é= "+num3);
     }
 }
