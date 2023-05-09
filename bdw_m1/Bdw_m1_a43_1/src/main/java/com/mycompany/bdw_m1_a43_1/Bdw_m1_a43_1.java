0ccb9d6df7fb357104329c07ab12c87ce6ec9925
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-05-08 23:03:34 -0300

    double

--- bdw_m1/Bdw_m1_a43_1/src/main/java/com/mycompany/bdw_m1_a43_1/Bdw_m1_a43_1.java
+++ bdw_m1/Bdw_m1_a43_1/src/main/java/com/mycompany/bdw_m1_a43_1/Bdw_m1_a43_1.java
@@ -1,16 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.bdw_m1_a43_1;
+import java.util.Scanner;
 
-/**
- *
- * @author dudah
- */
+/*
+1. Criar variáveis: peso, altura, imc
+2. Coletar informações com buffer
+3. Cálculo IMC: peso/altura^2
+4. Classificar o imc
+*/
+
 public class Bdw_m1_a43_1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        double peso, altura;
+        Scanner Entrada= new Scanner(System.in);
+        
+        System.out.println("Digite o peso: ");
+        peso=Entrada.nextDouble();
+        
+        System.out.println("Digite a altura: ");
+        altura=Entrada.nextDouble();
+        
+        double imc=((peso)/(altura*altura));
+        System.out.println("O IMC é: "+imc);
+        
+        
+                
+                
+                
+
     }
 }
