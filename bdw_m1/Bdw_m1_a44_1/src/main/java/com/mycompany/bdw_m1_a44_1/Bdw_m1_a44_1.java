7ac3a3339f4026fff1ee17513fd5291210569427
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-05-08 23:19:12 -0300

    create bdw m1 a44 1

---
+++ bdw_m1/Bdw_m1_a44_1/src/main/java/com/mycompany/bdw_m1_a44_1/Bdw_m1_a44_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bdw_m1_a44_1;
+
+/**
+ *
+ * @author dudah
+ */
+public class Bdw_m1_a44_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
