16305e1723bcebc632cb2e6cfb17109372213723
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-05-09 06:48:08 -0300

    Update Bdw_m1_tp_1.java

--- bdw_m1/bdw_m1_tp_1/src/main/java/com/mycompany/bdw_m1_tp_1/Bdw_m1_tp_1.java
+++ bdw_m1/bdw_m1_tp_1/src/main/java/com/mycompany/bdw_m1_tp_1/Bdw_m1_tp_1.java
@@ -1,9 +1,22 @@
 package com.mycompany.bdw_m1_tp_1;
-
-
+import java.util.Scanner;
 public class Bdw_m1_tp_1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int num1, num2;
+        double div=0;
+        boolean ok=false;
+        Scanner Entrada= new Scanner (System.in);
+        'System. out .println("Digite um numero inteiro maior que 100 (ex: 150).");
+        Num1=Entrada.nextInt();
+
+        System. out .println("Digite um número inteiro menor que 10 (ex: 6)");
+        num2=Entrada.nextInt();
+
+        
+        
+        
+
+
     }
 }
