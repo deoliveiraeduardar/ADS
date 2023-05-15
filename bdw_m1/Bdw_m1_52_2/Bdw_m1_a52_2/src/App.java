bc45493de0ff05df7f5f7acce5a69888ec1db506
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-05-15 13:54:12 -0300

    create

---
+++ bdw_m1/Bdw_m1_52_2/Bdw_m1_a52_2/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Imprimir lista de 10 a 1, usando ofr");
+        for(int x=10; x<=10; x--);
+        System.out.println("Número= "+x);
+    }
+}
