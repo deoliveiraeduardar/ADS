057d3fb08f6eaa4c8348b2a7554e845f51145eea
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-05-15 14:02:15 -0300

    java class

--- bdw_m1/Bdw_m1_52_2/Bdw_m1_a52_2/src/App.java
+++ bdw_m1/Bdw_m1_52_2/Bdw_m1_a52_2/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Imprimir lista de 10 a 1, usando ofr");
-        for(int x=10; x<=10; x--);
-        System.out.println("Número= "+x);
+        for(int x=10; x>=1; x--){
+        System.out.println("Número= " +x);  
     }
+}
 }
