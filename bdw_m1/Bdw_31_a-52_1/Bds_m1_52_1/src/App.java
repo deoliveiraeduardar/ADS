1ba2486eecdcb748af475e1d30c7515a13266d8c
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-05-15 13:48:59 -0300

    list

--- bdw_m1/Bdw_31_a-52_1/Bds_m1_52_1/src/App.java
+++ bdw_m1/Bdw_31_a-52_1/Bds_m1_52_1/src/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Criar programa para imprimir, a partir do for, de 1 a 10");
         for(int x=1; x<=10; x++){
-            System.out.println("Número = "+x);
+            System.out.println("Número= "+x);
         }
     }
 }
