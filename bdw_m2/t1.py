db8d069672c4ed44cacacc9cf287e68386140ba6
Eduarda Rosa de Oliveira <106114423+deoliveiraeduardar@users.noreply.github.com>
2023-05-22 22:06:00 -0300

    create

---
+++ bdw_m2/t1.py
@@ -0,0 +1,9 @@
+#implicação Se..então
+def implicacao(p, q):
+  return #IMPLEMENTE#
+
+print("p    q    a")
+for p in [True, False]:
+  for q in [True, False]:
+    a = implicacao(p, q)
+    print(p, q, a)
